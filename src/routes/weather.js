1ab811f1c361b0dbf296f818eef250a2f03e97e5
Abderrahmane <ar.boulahdour@outlook.com>
2021-03-29 13:39:21 +0100

    commit app

---
+++ src/routes/weather.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const weatherRouter = express.Router()
+const axios = require('axios')
+require('dotenv').config()
+
+weatherRouter.get('', async(req, res) => {
+    
+    var date_ob = new Date();
+    var day = ("0" + date_ob.getDate()).slice(-2);
+    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    var year = date_ob.getFullYear();
+    
+    var date = day + "-" + month + "-" + year
+
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Ain Naadja&appid=${process.env.API_KEY}`;
+        // `https://raddy.co.uk/wp-json/wp/v2/posts/`  
+        const weatherAPI = await axios.get(url)
+        console.log("Weather data: ", weatherAPI.data)
+        res.render('weather', { weather : weatherAPI.data, date: date })
+    } catch (err) {
+        if(err.response) {
+            res.render('weather', { weather : null, date: date })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.request) {
+            res.render('weather', { weather : null, date: date })
+            console.log(err.request)
+            alert(err.request)
+        } else {
+            res.render('weather', { weather : null, date: date })
+            console.error('Error', err.message)
+            alert(err.message)
+        }
+    } 
+})
+
+
+weatherRouter.post('', async(req, res) => {
+    
+    let search = req.body.search
+    
+    var date_ob = new Date();
+    var day = ("0" + date_ob.getDate()).slice(-2);
+    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    var year = date_ob.getFullYear();
+    
+    var date = day + "-" + month + "-" + year
+
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${process.env.API_KEY}`;
+        // `https://raddy.co.uk/wp-json/wp/v2/posts/`  
+        const weatherAPI = await axios.get(url)
+        console.log("Weather data: ", weatherAPI.data)
+        res.render('weather', { weather : weatherAPI.data, date: date })
+    } catch (err) {
+        if(err.response) {
+            res.render('weather', { weather : null, date: date })
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.request) {
+            res.render('weather', { weather : null, date: date })
+            console.log(err.request)
+            alert(err.request)
+        } else {
+            res.render('weather', { weather : null, date: date })
+            console.error('Error', err.message)
+            alert(err.message)
+        }
+    } 
+})
+
+module.exports = weatherRouter 
